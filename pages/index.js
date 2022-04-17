0d5e3d71ea44763765f60b09e0ed8e99e625013d
Kaleb Berry <kaleb.berry@outlook.com>
2022-04-17 16:24:22 -0500

    based is done

--- pages/index.js
+++ pages/index.js
@@ -1,61 +1,101 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faGithubAlt, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { fa1, faExternalLink, faFolder } from '@fortawesome/free-solid-svg-icons';
+import { faFolderClosed, faHeart } from '@fortawesome/free-regular-svg-icons';
+
 function HomePage() {
 	return (
 		<>
-			<header className='hero'>
+			<header className='hero '>
 				<div className='container'>
-					<h1>Hello! I'm Kaleb, a developer based in United States.</h1>
-					<h2>Front-End Developer</h2>
+					<h1>
+						Hello! I'm <span class='accent'> Kaleb</span>, <br /> A front-end developer based in
+						United States.
+					</h1>
 					<p>
-						An aesthetic web developer and design advocate. I strive to help others to achieve thier
-						dreams through my own passions and creativity.
+						With proven experience in helping companies create a better experience with their brand,
+						software, and maintain a better codebase. I am capable of learning and adapting to
+						technologies, and teams. Eager to tackle more problems and create using modern
+						technologies using skills like JavaScript (ES6+), Next JS, Contentful, APIs and more.
 					</p>
-					<button>View Github</button>
-					<button>View LinkedIn</button>
+					<ul>
+						<li>
+							<a role='button' href='https://github.com/Sorumeiji' target='_blank'>
+								View on Github
+							</a>
+						</li>
+						<li>
+							<a
+								role='button-outline'
+								href='https://www.linkedin.com/in/kalebgberry/'
+								target='_blank'>
+								View on LinkedIn
+							</a>
+						</li>
+					</ul>
 				</div>
 			</header>
 
-			<section>
+			<section className='projects'>
 				<div className='container'>
-					<div className='projects'>
+					<div className='projectsTitle'>
 						<h1>Projects & Work.</h1>
 						<p>Preview of completed and ongoing projects:</p>
 					</div>
 					<div className='projectGallery'>
 						<div className='projectGallery-item'>
-							<h2>Torch AI</h2>
+							<h2>
+								<span className='accent'>01.</span> Torch.AI
+							</h2>
 							<p>
 								A public facing website for Torch AI. Created with Next JS, and Contentful. I was
 								able to help design, create and structure the website for the company.
 							</p>
-							<p>Html5 Scss NextJS Contentful Animation Vercel</p>
+							<p class='stack'>Next JS SCSS Contentful Vercel</p>
 							<ul>
 								<li>
-									<a href='#'>Link</a>
+									<a role='icon' href='#'>
+										<FontAwesomeIcon icon={faGithub} />
+									</a>
 								</li>
 								<li>
-									<a href='#'>Link</a>
+									<a role='icon' href='#'>
+										<FontAwesomeIcon icon={faExternalLink} />
+									</a>
 								</li>
 							</ul>
 						</div>
 
 						<div className='projectGallery-item'>
-							<h2>Nyacchii</h2>
+							<h2>
+								<span className='accent'>02.</span> Nyacchii
+							</h2>
 							<p>
 								A portoflio website for Nyacchii, an professional digital artist and Twitch partner.
 							</p>
-							<p>Html5 Scss NextJS Vercel</p>
+							<p class='stack'>SCSS Next JS Vercel</p>
 							<ul>
 								<li>
-									<a href='#'>Link</a>
+									<a role='icon' href='#'>
+										<FontAwesomeIcon icon={faGithub} />
+									</a>
 								</li>
 								<li>
-									<a href='#'>Link</a>
+									<a role='icon' href='#'>
+										<FontAwesomeIcon icon={faExternalLink} />
+									</a>
 								</li>
 							</ul>
 						</div>
 					</div>
 				</div>
 			</section>
+
+			<footer className='footer'>
+				<div className='container'>
+					<p>Designed and Built by Kaleb Berry.</p>
+				</div>
+			</footer>
 		</>
 	);
 }
